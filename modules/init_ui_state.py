f43c9aff3e6bfc4a5f4fa509775169e8c4cb580b
Nurul Yakim Kazal <kazal.yakim@gmail.com>
2026-06-04 14:24:14 +0800

    Add session state for ML parameters in UI

--- modules/init_ui_state.py
+++ modules/init_ui_state.py
@@ -3,9 +3,22 @@ import streamlit as st
 
 def init_ui_state():
     defaults = {
+        # =========================
+        # MAP UI
+        # =========================
         "view_mode": "Scatter",
-        "mag_range": (2.5, 7.5),
-        "max_depth": 300
+        "mag_range": (3, 7.5),
+        "max_depth": 300,
+
+        # =========================
+        # ML UI
+        # =========================
+        "ml_algorithm": "HDBSCAN",
+        "dbscan_eps_km": 30,
+        "dbscan_min_samples": 5,
+        "hdb_min_cluster_size": 8,
+        "hdb_min_samples": 5,
+        "show_noise": True,
     }
 
     for key, value in defaults.items():
