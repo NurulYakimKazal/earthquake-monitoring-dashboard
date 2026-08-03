ad47851161630e4a0dee20cba1ee98b2ac8eb62f
Nurul Yakim Kazal <kazal.yakim@gmail.com>
2026-08-04 00:07:52 +0800

    refine the zoom level of the map

--- modules/clustering_map.py
+++ modules/clustering_map.py
@@ -113,7 +113,7 @@ def render_ml_cluster_map(dff, show_noise):
     fig.update_layout(
         map=dict(
             style="carto-darkmatter",
-            zoom=1.5,
+            zoom=1.47,
             center=dict(
                 lat=0,
                 lon=0,
--- modules/earthquake_map.py
+++ modules/earthquake_map.py
@@ -173,7 +173,7 @@ def render_earthquake_map(filtered_dff, unfiltered_dff):
     fig.update_layout(
         map=dict(
             style="carto-darkmatter",
-            zoom=1.45,
+            zoom=1.47,
             center=dict(
                 lat=0,
                 lon=0,
